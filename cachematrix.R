03bf017241103000e44bc57fd3765068ecba54ae
moocarme <moocarme@gmail.com>
2015-07-08 23:23:25 -0400

    Create functions for caching a matrix and solving the inverse
    2 functions, 1 that initializes and caches a matrix, storing the
    variables to the workspace. The second takes the cached matrix and
    computes the inverse.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first of these functions stores a matrix in the cache, then the second 
+## takes the matrix from the cache and computes the inverse. This is useful
+## for the computationally intensive inverse function and to have control on 
+##when it operates
 
-## Write a short comment describing this function
+## makechacheMatrix initializes a matirx and stores the variables in the workspace
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      ## Initializes a matirx and stores the variables in the workspace
+      
+      m <- NULL       ## Intialize matrix
+      
+      ## Create set function 
+      set <- function(y) {
+            x <<- y
+            m <<- NULL
+      }
+      
+      ## Create get funtion
+      get <- function() x
+      setInverse <- function(solve) m <<- solve  ## Set inverse function
+      getInverse <- function() m                 ## Get inverse function
+      
+      ## Create list containing all functions
+      list(set = set, get = get,
+           setInverse = setInverse,
+           getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      ## Returns a matrix that is the inverse of x
+      
+      ## if the inverse is already solved do not compute
+      m <- x$getInverse()
+      if(!is.null(m)) {
+            message("getting cached data")
+            return(m)
+      }
+      
+      data <- x$get()  ## Get the matrix
+      m <- solve(data, ...)  ## Solve the matrix
+      x$setInverse(m)
+      m    ## return m from the function
 }
